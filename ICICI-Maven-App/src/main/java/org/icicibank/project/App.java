cec4d37fca080325a58f64acbf4ad037d8fceca5
GVKR1409 <43790208+GVKR1409@users.noreply.github.com>
2022-06-22 20:21:13 +0530

    Update App.java

--- ICICI-Maven-App/src/main/java/org/icicibank/project/App.java
+++ ICICI-Maven-App/src/main/java/org/icicibank/project/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-	System.out.rpintln("My Name is Vinod");
+	System.out.println("My Name is Vinod");
     }
 }
