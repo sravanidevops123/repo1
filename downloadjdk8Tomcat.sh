a7c8d7b2c880ec518fcf018bab6faabad386a436
GVKR1409 <43790208+GVKR1409@users.noreply.github.com>
2022-07-22 18:11:48 +0530

    Added Shell script for downloading tomcat & JDK

---
+++ downloadjdk8Tomcat.sh
@@ -0,0 +1,6 @@
+sudo yum update && yum install java-1.8.0-openjdk -y
+
+cd /opt/
+curl -O https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.41/bin/apache-tomcat-8.5.41.tar.gz
+tar -xzvf apache-tomcat-8.5.41.tar.gz
+mv apache-tomcat-8* tomcat8.5.41
