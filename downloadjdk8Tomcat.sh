7ec3b46535b5a078381661ee453073f730d73310
GVKR1409 <43790208+GVKR1409@users.noreply.github.com>
2022-07-22 19:22:44 +0530

    Update downloadjdk8Tomcat.sh

--- downloadjdk8Tomcat.sh
+++ downloadjdk8Tomcat.sh
@@ -1,4 +1,4 @@
-sudo yum update && yum install java-1.8.0-openjdk -y
+sudo yum update -y && sudo yum install java-1.8.0-openjdk -y
 
 cd /opt/
 curl -O https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.41/bin/apache-tomcat-8.5.41.tar.gz
