08a5385699e8fa2de4eca2bd514a57cfd8fcc64a
GVKR1409 <43790208+GVKR1409@users.noreply.github.com>
2022-07-27 10:55:26 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  default = "ami-08df646e18b182346"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+
+variable "key_name" {
+  default="myLap"
+}
+
+variable "vpc_id" {
+  default = "vpc-71d6021a"
+}
