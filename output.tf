b3f138da6e40174d8dad8b8e2f43b04980bac084
GVKR1409 <43790208+GVKR1409@users.noreply.github.com>
2022-08-01 12:29:33 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,6 +1,11 @@
 output "my_publi_ip" {
   value = aws_instance.web.public_ip
 }
+
+output "my_instance_id" {
+  value = aws_instance.web.id
+}
+
 output "my_publi_dns" {
   value = aws_instance.web.public_dns
 }
