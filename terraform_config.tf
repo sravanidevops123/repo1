9f3351d1fff1bc42813bd4811f1fe2b71da78201
GVKR1409 <43790208+GVKR1409@users.noreply.github.com>
2022-08-01 11:43:11 +0530

    Update terraform_config.tf

--- terraform_config.tf
+++ terraform_config.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mybucket-1213-2131"
-    key    = "terraform-deploy-through-jenkins/terraform.tfstate"
+    key    = "build-and-deploy-pipeline-terraform-ec2-ansible-1/terraform.tfstate"
     region = "ap-south-1"
     dynamodb_table = "mytable"
   }
